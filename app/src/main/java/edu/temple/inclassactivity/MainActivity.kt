15d5b02cd9720097f1b0413e160a12ee65c0fb20
svtslvtt <tup60276@temple.edu>
2024-10-15 20:12:25 -0400

    added container and instantiated fragment

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        val fragment = ImageDisplayFragment.newInstance(imageArray)
+
 
     }
 }
